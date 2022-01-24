ffa71fa95dd320527394dc1d6526fc901a20ba6e
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2022-01-24 15:05:42 -0500

    did some stuff

--- coronasafe_v3_backend.py
+++ coronasafe_v3_backend.py
@@ -14,7 +14,7 @@ import surrounding_density_calculator as surrounding_density
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-dotenv_path = Path('google_api_key.env')
+dotenv_path = Path('g_api_key.env')
 
 
 load_dotenv(dotenv_path=dotenv_path)
