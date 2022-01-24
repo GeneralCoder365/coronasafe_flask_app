a4efd360a997c0774e8e727157081f122658226f
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2022-01-24 13:54:30 -0500

    extension of previous debugging strategy

--- heat_maps.py
+++ heat_maps.py
@@ -28,103 +28,103 @@ from urllib.request import urlopen
 import json
 from datetime import datetime, time
 
-from github import Github, UnknownObjectException
-dotenv_path = Path('github_api_token.env')
-load_dotenv(dotenv_path=dotenv_path)
-GITHUB_API_TOKEN = str(os.getenv('GITHUB_API_TOKEN'))
-# print(GITHUB_API_TOKEN)
+# from github import Github, UnknownObjectException
+# dotenv_path = Path('github_api_token.env')
+# load_dotenv(dotenv_path=dotenv_path)
+# GITHUB_API_TOKEN = str(os.getenv('GITHUB_API_TOKEN'))
+# # print(GITHUB_API_TOKEN)
 
-github_object = Github(GITHUB_API_TOKEN)
-repository = github_object.get_user().get_repo('coronasafe_plotly_map_urls')
+# github_object = Github(GITHUB_API_TOKEN)
+# repository = github_object.get_user().get_repo('coronasafe_plotly_map_urls')
 
-def github_updater_us_case_map_embed_url(html_embed_url):
-    # path in the repository
-    filename = 'us_case_map_url.txt'
-    content = html_embed_url
-    # create with commit message
-    # file = repository.create_file(filename, "edit_file via PyGithub", content)
+# def github_updater_us_case_map_embed_url(html_embed_url):
+#     # path in the repository
+#     filename = 'us_case_map_url.txt'
+#     content = html_embed_url
+#     # create with commit message
+#     # file = repository.create_file(filename, "edit_file via PyGithub", content)
 
-    try:
-        file = repository.get_contents(filename)
+#     try:
+#         file = repository.get_contents(filename)
         
-        # update file syntax: repository.update_file(path, message, content, sha, branch=NotSet, committer=NotSet, author=NotSet)
-        repository.update_file(file.path, "update us case map url", content, file.sha, branch="main")
-    # Error raised: github.GithubException.UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/repos#get-repository-content"}
-    except UnknownObjectException:
-        file = repository.create_file(filename, "create us_case_map_url.txt", content)
+#         # update file syntax: repository.update_file(path, message, content, sha, branch=NotSet, committer=NotSet, author=NotSet)
+#         repository.update_file(file.path, "update us case map url", content, file.sha, branch="main")
+#     # Error raised: github.GithubException.UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/repos#get-repository-content"}
+#     except UnknownObjectException:
+#         file = repository.create_file(filename, "create us_case_map_url.txt", content)
 
-# tester code
-# print(github_updater_us_case_map_embed_url(make_us_case_map()))
+# # tester code
+# # print(github_updater_us_case_map_embed_url(make_us_case_map()))
 
-# makes US heat map (auto overwrites any existing one)
-def make_us_case_map():
-    ssl._create_default_https_context = ssl._create_unverified_context
-    response = urllib.request.urlopen('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
-    response1 = urllib.request.urlopen('https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv')
+# # makes US heat map (auto overwrites any existing one)
+# def make_us_case_map():
+#     ssl._create_default_https_context = ssl._create_unverified_context
+#     response = urllib.request.urlopen('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+#     response1 = urllib.request.urlopen('https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv')
 
-    url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-    df = pd.read_csv(url, converters={'fips': lambda x: str(x)})
+#     url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+#     df = pd.read_csv(url, converters={'fips': lambda x: str(x)})
 
-    url = "https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv"
-    df_abbrev = pd.read_csv(url)
+#     url = "https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv"
+#     df_abbrev = pd.read_csv(url)
 
-    last_date = df['date'].max()
-    df = df[ df['date'] == last_date]
-    # print(df['cases'].sum())
-    df = df.groupby('state')['cases'].sum().to_frame()
-    df = pd.merge(df, df_abbrev, left_on=df.index, right_on='State')
+#     last_date = df['date'].max()
+#     df = df[ df['date'] == last_date]
+#     # print(df['cases'].sum())
+#     df = df.groupby('state')['cases'].sum().to_frame()
+#     df = pd.merge(df, df_abbrev, left_on=df.index, right_on='State')
 
-    fig = px.choropleth(df, locations=df['Abbreviation'], color=df['cases'],
-                        locationmode="USA-states",
-                        # alternate colour scheme -> color_continuous_scale=px.colors.diverging.RdYlGn[::-1],
-                        # _r reverses the hot colour scheme
-                        color_continuous_scale="hot_r",
-                        range_color=(0, 4500000),
-                        scope="usa"
-                            )
+#     fig = px.choropleth(df, locations=df['Abbreviation'], color=df['cases'],
+#                         locationmode="USA-states",
+#                         # alternate colour scheme -> color_continuous_scale=px.colors.diverging.RdYlGn[::-1],
+#                         # _r reverses the hot colour scheme
+#                         color_continuous_scale="hot_r",
+#                         range_color=(0, 4500000),
+#                         scope="usa"
+#                             )
 
-    fig.update_layout(paper_bgcolor="#4E5D6C")
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, geo=dict(bgcolor= '#4E5D6C',lakecolor='#4E5D6C'))
-    # fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+#     fig.update_layout(paper_bgcolor="#4E5D6C")
+#     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, geo=dict(bgcolor= '#4E5D6C',lakecolor='#4E5D6C'))
+#     # fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
     
-    # fig.write_html('maps/us_map.html')
-    # ! plot <-- plotly.offline.plot
-    # plot(fig, filename = 'templates/us_map.html', auto_open=False)
+#     # fig.write_html('maps/us_map.html')
+#     # ! plot <-- plotly.offline.plot
+#     # plot(fig, filename = 'templates/us_map.html', auto_open=False)
 
-    plotly_url = py.plot(fig, filename='us_case_map', auto_open=False, sharing='public')
+#     plotly_url = py.plot(fig, filename='us_case_map', auto_open=False, sharing='public')
 
-    # html_embed_code = tls.get_embed(plotly_url)
-    # print(html_embed_code)
+#     # html_embed_code = tls.get_embed(plotly_url)
+#     # print(html_embed_code)
     
-    plotly_html_embed_url = plotly_url[:-1] + ".embed"
+#     plotly_html_embed_url = plotly_url[:-1] + ".embed"
     
-    github_updater_us_case_map_embed_url(plotly_html_embed_url)
+#     github_updater_us_case_map_embed_url(plotly_html_embed_url)
     
-    return plotly_html_embed_url
+#     return plotly_html_embed_url
 
 # # tester code
 # print(make_us_case_map())
 
 
-def get_us_case_map():
-    # path in the repository
-    filename = 'us_case_map_url.txt'
+# def get_us_case_map():
+#     # path in the repository
+#     filename = 'us_case_map_url.txt'
 
-    # create with commit message
-    # file = repository.create_file(filename, "edit_file via PyGithub", content)
+#     # create with commit message
+#     # file = repository.create_file(filename, "edit_file via PyGithub", content)
 
-    try:
-        file = repository.get_contents(filename)
+#     try:
+#         file = repository.get_contents(filename)
         
-        # read file
-        us_case_map_html_embed_url = file.decoded_content.decode()
+#         # read file
+#         us_case_map_html_embed_url = file.decoded_content.decode()
     
-    # Error raised: github.GithubException.UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/repos#get-repository-content"}
-    except UnknownObjectException:
-        us_case_map_html_embed_url = make_us_case_map()
+#     # Error raised: github.GithubException.UnknownObjectException: 404 {"message": "Not Found", "documentation_url": "https://docs.github.com/rest/reference/repos#get-repository-content"}
+#     except UnknownObjectException:
+#         us_case_map_html_embed_url = make_us_case_map()
 
     
-    return us_case_map_html_embed_url
+#     return us_case_map_html_embed_url
 
 # tester code
 # print(get_us_case_map())
